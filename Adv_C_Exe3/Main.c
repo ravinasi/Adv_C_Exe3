43b5624b12f36e33a7140f840d903fb6e06a35ad
ravinasi <ravin.asi55@gmail.com>
2022-04-12 17:20:15 +0300

    first version of program

--- Adv_C_Exe3/Main.c
+++ Adv_C_Exe3/Main.c
@@ -1,8 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 #include "Stack.h"
 #include "Queue.h"
+#define SIZE 100
 
 void main()
 {
-	// add your code here
+	int choice;
+	char string[SIZE];
+	printf("enter a sentence\n");
+	gets(string);
+	int stringSize = strlen(string);
+	char* sentence = (char*)malloc(sizeof(char)* stringSize + 1);
+	if (!sentence)
+	{
+		printf("allocation faild");
+		exit(1);
+	}
+	Stack* s = (Stack*)malloc(sizeof(Stack));
+	if (!s)
+	{
+		printf("allocation failed");
+		exit(1);
+	}
+	strcpy(sentence, string);
+	printf("what would you want to do?\n");
+	printf("1- flip between hashes\n");
+	printf("2 - check if a string is palindrom\n");
+	printf("3- rotate a stack\n");
+	printf("4 - exit\n");
+	printf("enter your choice: ");
+	scanf(" %d", &choice);
+	while (choice != 4)
+	{
+		switch (choice)
+		{
+		case (1):
+			flipBetweenHashes(sentence);
+		break;
+		case (2):
+		{
+			int i = 0;
+			while (sentence)
+			{
+				push(s, sentence[i]);
+			}
+			int res = isPalindrome(s);
+			if (res)
+				printf("the sentence is palindrom\n");
+			else printf("the sentence is not palindrom\n");
+		}
+		break;
+		case (3):
+		{
+			int i = 0;
+			while (sentence)
+			{
+				push(s, sentence[i]);
+			}
+			Stack* temp = s;
+			printf("enter a number: ");
+			scanf(" %d", &i);
+			rotateStack(s, i);
+			printf("the new sentence is: ");
+			while (temp != NULL)
+			{
+				printf("%c", temp->head->data);
+				temp->head = temp->head->next;
+			}
+		}
+		break;
+		default:
+			printf(" invalid selection, try again");
+		break;
+		}
+		scanf(" %d", &choice);
+	}
+	printf("you chose to exit");
+	destroyStack(s);
 }
--- Adv_C_Exe3/Stack.c
+++ Adv_C_Exe3/Stack.c
@@ -1,46 +1,220 @@
+﻿#include <stdio.h>
 #include "Stack.h"
 
+Stack* intervesionWord(Stack* s);
 /***************** Stack ADT Implementation *****************/
 
-void initStack(Stack* s)
+void initStack(Stack* s)                         
 {
-	// add your code here
+	s->head = NULL;
 }
 
 void destroyStack(Stack* s)
 {
-	// add your code here
+	while (s->head != NULL)
+		pop(s);
 }
 
 void push(Stack* s, char data)
 {
-	// add your code here
+	charNode* newnode = (charNode*)malloc(sizeof(charNode));
+	if (!newnode)
+	{
+		printf("push: allocation faild\n");
+		return;
+	}
+	newnode->data = data;
+	s->head = addToHead(s->head, newnode);
+}
+
+charNode* addToHead(charNode *head, charNode *toadd)
+{
+	toadd->next = head;
+	head = toadd;
+	return head;
 }
 
 char pop(Stack* s)
 {
-	// add your code here
+	char res = s->head->data;
+	charNode* temp = s->head;
+	s->head = s->head->next;
+	free(temp);
+	return res;
 }
-
 
 int isEmptyStack(const Stack* s)
 {
-	// add your code here
+	if (s->head == NULL)
+		return 1;
+	return 0;
 }
 
 /*************** Functions using stacks - Implementation/definition **************************/
 
 void flipBetweenHashes(const char* sentence)
 {
-	// add your code here
+	int i=0, hashAmount = 0;	
+	Stack* tempstack = (Stack*)malloc(sizeof(Stack));
+	if (!tempstack)
+	{
+		printf("\nallocation failed");
+		exit(1);
+	}
+	initStack(tempstack);
+	Stack* s = (Stack*)malloc(sizeof(Stack));
+	if (!s)
+	{
+		printf("\nallocation failed");
+		exit(1);
+	}
+	initStack(s);
+	if (sentence == NULL)
+	{
+		printf("the string is empty");
+		return;
+	}
+	
+	while (sentence[i] != NULL)// put into stack
+	{
+		push(tempstack, sentence[i++]);
+	}
+	Stack* temp = tempstack;
+	while (temp->head != NULL)// flip and count hashes
+	{
+		push(s, temp->head->data);
+		if (temp->head->data == '#')
+			hashAmount++;
+		temp->head = temp->head->next;
+	}
+	destroyStack(tempstack);
+	if (hashAmount % 2 == 1) // odd number of hashes
+	{
+		printf("odd number of hashes");
+		return;
+	}
+	printf("the sentnce is:\n");
+	while (s->head != NULL)
+	{
+		if (s->head->data == '#')
+		{
+			s->head = intervesionWord(s);// flip and print func
+		}
+		else printf("%c", s->head->data);
+		s->head = s->head->next;
+	}
+	destroyStack(s);
+}
+
+Stack* intervesionWord(Stack* s)
+{
+	s->head = s->head->next;
+	Stack* newstack = (Stack*)malloc(sizeof(Stack));
+	if (!newstack)
+	{
+		printf("\nallocation failed");
+		exit(1);
+	}
+	while (s->head->data != '#')
+	{
+		push(newstack, s->head->data);
+		s->head = s->head->next;
+	}
+	while (newstack->head != NULL)
+	{
+		printf("%c", newstack->head->data);
+		newstack->head = newstack->head->next;
+	}
+	free(newstack);
+	return s->head->next;
 }
 
 int isPalindrome(Stack* s)
 {
-	// add your code here
+	// לבדוק 5 ו-6
+	Stack* temp;
+	Stack* smallstack = (Stack*)malloc(sizeof(Stack));
+	if (!smallstack)
+	{
+		printf("allocation error");
+		return;
+	}
+	initStack(smallstack);
+	int count = 0;
+	temp = s;
+	while (temp != NULL)// check stack size
+		count++;
+	if (count)// if there is one char in the sentence
+		return 1;
+	for (int i = 0; i < count/2; i++)// push into small stack
+	{
+		push(smallstack, temp->head);
+		temp->head = temp->head->next;
+	}
+	if (count % 2 == 1) // odd size of stack
+	{
+		temp->head = temp->head->next;
+	}
+	for (int i = 0; i < count/2; i++)// equal test between the stacks
+	{
+		if (smallstack->head != temp->head)
+			return 0;
+		smallstack->head = smallstack->head->next;
+		temp->head = temp->head->next;
+	}
+	free(smallstack);
+	return 1;
 }
 
 void rotateStack(Stack* s, int n)
 {
-	// add your code here
+	//לבדוק על 5 ו-6
+	if (n <= 0)// if n is negetiv
+	{
+		printf("can't rotate - negetiv number");
+		return;
+	}
+	Stack* temp;
+	int size = 0;
+	temp = s;
+	while (temp != NULL)// check stack size
+		size++;
+	if (n >= size)
+		return;
+	Stack* helpStack = (Stack*)malloc(sizeof(Stack));
+	if (!helpStack)
+	{
+		printf("allocation error");
+		return;
+	}
+	Stack* Newstack = (Stack*)malloc(sizeof(Stack));
+	if (!Newstack)
+	{
+		printf("allocation error");
+		return;
+	}
+	for (int i = 0; i < size - n; i++)// קידום לאיבר
+	{
+		push(helpStack, s->head->data);
+		pop(s);
+		s->head = s->head->next;
+	}
+	for (int i = 0; i < n; i++)// push to newstack
+	{
+		push(Newstack, s->head->data);
+		pop(s);
+		s->head = s->head->next;
+	}	
+	while (helpStack != NULL)
+	{
+		push(s, helpStack->head->data);
+		pop(helpStack);
+		helpStack->head = helpStack->head->next;
+	}
+	while (Newstack != NULL)
+	{
+		push(s, Newstack->head->data);
+		pop(Newstack);
+		Newstack->head = Newstack->head->next;
+	}
 }
